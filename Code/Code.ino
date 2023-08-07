4cde5a8a40fbbbadd96dd1fa7ad99b06eb5b4404
Prateek Singh <92904643+PrateekSinghRajput@users.noreply.github.com>
2023-08-08 02:26:30 +0530

    Add files via upload

---
+++ Code/Code.ino
@@ -0,0 +1,37 @@
+//Prateek
+//wwww.prateeks.in
+//https://www.youtube.com/c/JustDoElectronics/videos
+
+#include <LiquidCrystal.h>
+LiquidCrystal lcd(13, 12, 11, 10, 9, 8);
+
+float cf = 19.5;
+int ffs1 = A0;
+int ffsdata = 0;
+float vout;
+
+void setup() {
+  Serial.begin(9600);
+  lcd.begin(16, 2);
+  lcd.print("WWW.PRATEEKS.IN");
+  delay(1000);
+  pinMode(ffs1, INPUT);
+}
+
+void loop() {
+
+  ffsdata = analogRead(ffs1);
+  vout = (ffsdata * 5.0) / 1023.0;
+  vout = vout * cf;
+  Serial.print("Flexi Force sensor: ");
+  lcd.setCursor(0, 0);
+  lcd.print("Force Range: ");
+  delay(100);
+  lcd.setCursor(0, 1);
+  lcd.print(vout, 3);
+  lcd.print("");
+  lcd.print(" Gram");
+  Serial.print(vout, 3);
+  Serial.println("");
+  delay(100);
+}
